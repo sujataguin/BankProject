25ad14bfb81e39ad8997b4874917545834283422
sujataguin <sujataguin1@gmail.com>
2017-01-26 14:48:30 -0500

    Create Test3.java

---
+++ BankofAmericaProject/src/PKG1/Test3.java
@@ -0,0 +1,16 @@
+package PKG1;
+
+
+
+public class Test3{
+
+
+
+	public static void main(String[] args) {
+
+	
+
+		//this is comment on Test3
+
+		System.out.println("This is Welcome to Practice JAVA");
+
